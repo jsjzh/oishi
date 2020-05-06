57f47f826645fe6a3b87335ffe8c2c0c9df72f4d
jsjzh <kimimi_king@163.com>
2020-05-06 11:47:42 +0800

    version

--- packages/cli/src/index.js
+++
@@ -1,8 +0,0 @@
-import CliCore from '@oishi/cli-core';
-import createCli from './plugins/create:cli';
-import createPlg from './plugins/create:plg';
-import createTs from './plugins/create:ts';
-export default new CliCore({
-    root: process.cwd(),
-    plugins: [createCli, createPlg, createTs],
-});
