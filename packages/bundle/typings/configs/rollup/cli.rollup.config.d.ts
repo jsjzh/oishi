2d1713538b1ea5d1e5c7953c44beb917d2efa164
jsjzh <kimimi_king@163.com>
2020-11-13 17:19:43 +0800

    update version

--- packages/bundle/typings/configs/rollup/cli.rollup.config.d.ts
+++ packages/bundle/typings/configs/rollup/cli.rollup.config.d.ts
@@ -1,6 +1,14 @@
+import { RollupJsonOptions as jsonOptions } from '@rollup/plugin-json';
+import { Options as resolveOptions } from '@rollup/plugin-node-resolve';
+import { RollupCommonJSOptions as commonjsOptions } from '@rollup/plugin-commonjs';
+import { MinifyOptions } from 'terser';
 import T from '../../types';
 export interface ICliOptions {
     pkg: Partial<T.IBasePkg> & Record<keyof any, any>;
+    json: jsonOptions;
+    resolve: resolveOptions;
+    terser: Omit<MinifyOptions, 'sourceMap'>;
+    commonjs: commonjsOptions;
 }
 declare const _default: (options: ICliOptions) => () => Promise<{
     input: string;
--- packages/bundle/typings/configs/rollup/lib.rollup.config.d.ts
+++ packages/bundle/typings/configs/rollup/lib.rollup.config.d.ts
@@ -1,7 +1,14 @@
+import { RollupJsonOptions as jsonOptions } from '@rollup/plugin-json';
+import { Options as resolveOptions } from '@rollup/plugin-node-resolve';
+import { RollupCommonJSOptions as commonjsOptions } from '@rollup/plugin-commonjs';
+import { MinifyOptions } from 'terser';
 import T from '../../types';
 export interface ILibOptions {
     pkg: Partial<T.IBasePkg> & Record<keyof any, any>;
-    terser: Record<keyof any, any>;
+    json: jsonOptions;
+    resolve: resolveOptions;
+    terser: Omit<MinifyOptions, 'sourceMap'>;
+    commonjs: commonjsOptions;
 }
 declare const _default: (options: ILibOptions) => () => Promise<{
     input: string;
