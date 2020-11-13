b6054f50d574ddc7f33702ffa34384089790fbaf
jsjzh <kimimi_king@163.com>
2020-11-13 16:56:21 +0800

    update version

--- packages/bundle/typings/configs/rollup/lib.rollup.config.d.ts
+++ packages/bundle/typings/configs/rollup/lib.rollup.config.d.ts
@@ -1,6 +1,7 @@
 import T from '../../types';
 export interface ILibOptions {
     pkg: Partial<T.IBasePkg> & Record<keyof any, any>;
+    terser: Record<keyof any, any>;
 }
 declare const _default: (options: ILibOptions) => () => Promise<{
     input: string;
